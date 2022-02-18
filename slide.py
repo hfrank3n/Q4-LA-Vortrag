a010182ae6123a64b0df4f8ddbd34750993dd997
Daniel-Dietzler <daniel.dietzler@web.de>
2022-02-18 20:23:15 +0100

    Add Shear summary slide

--- slide.py
+++ slide.py
@@ -1,3 +1,4 @@
+from operator import eq
 from manim import *
 from numpy import *
 
@@ -83,7 +84,7 @@ class LineareTransformationenSlide(Scene):
         left_grid[2].set_color(ORANGE)
 
         transformation_arrow = MathTex(r"\longleftrightarrow", font_size=80)
-        transformation_text = MathTex("T")
+        transformation_text = Tex("T")
         transformation_text.next_to(transformation_arrow, UP, buff=-0.06)
         transformation_label = VGroup(
             transformation_arrow, transformation_text)
@@ -108,3 +109,97 @@ class LineareTransformationenSlide(Scene):
 
         self.add(text, brace, function_equation, matrix_equation)
         self.add(left_grid, right_grid, transformation_label, matrix)
+
+
+class EinfacheTransformationenSlide(Scene):
+    def construct(self):
+        grid1 = Axes(
+            x_range=[-0.8, 2.5, 1],
+            y_range=[-0.8, 2.5, 1],
+            x_length=3.3,
+            y_length=3.3,
+            axis_config={
+                "include_numbers": True,
+                "font_size": 24,
+                "tip_length": 0.2
+            },
+            tips=True,
+        )
+
+        v1 = Vector([1, 0], tip_length=0.25, color=ORANGE)
+        v1_label = v1.coordinate_label()
+        v1_label.scale(0.5)
+        v1_label.next_to(v1.get_end(), UP + RIGHT)
+
+        v2 = Vector([0, 1], tip_length=0.25, color=ORANGE)
+        v2_label = v2.coordinate_label()
+        v2_label.scale(0.5)
+        v2_label.next_to(v2.get_end(), RIGHT, buff=MED_SMALL_BUFF)
+
+        v3 = Vector([1, -1], tip_length=0.25, color=BLUE)
+        v3_label = v3.coordinate_label(color=BLUE)
+        v3_label.scale(0.5)
+        v3_label.next_to(v3.get_end(), RIGHT)
+
+        vGroup = VGroup(v1, v2, v3, v1_label, v2_label, v3_label)
+        grid1.add(vGroup)
+
+        grid1[2].shift(grid1.get_origin())
+
+        grid2 = Axes(
+            x_range=[-0.8, 2.5, 1],
+            y_range=[-0.8, 2.5, 1],
+            x_length=3.3,
+            y_length=3.3,
+            axis_config={
+                "include_numbers": True,
+                "font_size": 24,
+                "tip_length": 0.2
+            },
+            tips=True,
+        )
+
+        v4 = Vector([2, 0], tip_length=0.25, color=ORANGE)
+        v4_label = v4.coordinate_label()
+        v4_label.scale(0.5)
+        v4_label.next_to(v4.get_end(), UP)
+
+        v5 = Vector([0, 2], tip_length=0.25, color=ORANGE)
+        v5_label = v5.coordinate_label()
+        v5_label.scale(0.5)
+        v5_label.next_to(v5.get_end(), RIGHT)
+
+        v6 = Vector([2, -2], tip_length=0.25, color=BLUE)
+        v6_label = v6.coordinate_label(color=BLUE)
+        v6_label.scale(0.5)
+        v6_label.next_to(v6.get_end(), UP + RIGHT)
+
+        vGroup2 = VGroup(v5, v4, v6, v4_label, v5_label, v6_label)
+        grid2.add(vGroup2)
+
+        grid2[2].shift(grid2.get_origin())
+
+        grid1.to_corner(UL, buff=1)
+        grid2.next_to(grid1, RIGHT)
+        grid2.to_edge(UP, buff=1)
+
+        equation_matrix_mul = MathTex(
+            r"T = \begin{bmatrix} 2 & 0 \\ 0 & 2 \end{bmatrix} = 2 \cdot \begin{bmatrix} 1 & 0 \\ 0 & 1 \end{bmatrix}", font_size=40)
+
+        equation_matrix_mul.to_corner(UR, buff=1)
+
+        other_equation = MathTex(
+            r"""\vec{v'} &= T \cdot \vec{v} \\ %
+                &= \begin{bmatrix} 2 & 0 \\ 0 & 2 \end{bmatrix} \cdot \begin{bmatrix} 1 \\ -1 \end{bmatrix} \\ %
+                &= \begin{bmatrix} 1 \cdot 2 - 1 \cdot 0 \\ 1 \cdot 0 - 2 \end{bmatrix} = \begin{bmatrix} 2 \\ -2 \end{bmatrix}""",
+            color=BLUE, font_size=40)
+
+        other_equation.next_to(equation_matrix_mul, DOWN)
+
+        label_grid1 = grid1.get_axis_labels(
+            Tex("x").scale(0.7), Tex("y").scale(0.7))
+        label_grid2 = grid2.get_axis_labels(
+            Tex("x").scale(0.7), Tex("y").scale(0.7))
+
+        self.add(grid1, grid2, equation_matrix_mul,
+                 other_equation, label_grid1, label_grid2)
