55fc830ee4a1d7055bd0dd357a057b7be1041c21
Daniel-Dietzler <daniel.dietzler@web.de>
2022-02-19 15:37:53 +0100

    Implement shear animation in 3d coordinate system
    Co-authored-by: Hannah Franken <hfrank3n@users.noreply.github.com>

--- threeDSlide.py
+++ threeDSlide.py
@@ -2,22 +2,42 @@ from manim import *
 from numpy import *
 
 
-class RotationAnimationSlide(ThreeDScene):
+class ShearAnimationSlide(ThreeDScene):
     def construct(self):
+        matrix = [[2, 0, 0], [0, 2, 0], [0, 0, 2]]
+
         axes = ThreeDAxes(
             x_range=[-5, 5, 1],
             y_range=[-5, 5, 1],
-            z_range=[-3, 3, 1]
+            z_range=[-3, 3, 1],
+            axis_config={
+                "include_numbers": True
+            }
         )
 
-        v1 = Vector([1, 0, 0], color=ORANGE)
-        v2 = Vector([0, 1, 0], color=ORANGE)
-        v3 = Vector([0, 0, 1], color=ORANGE)
+        v1 = Vector([1, 0, 0], color=ORANGE, tip_length=0.25)
+        v2 = Vector([0, 1, 0], color=ORANGE, tip_length=0.25)
+        v3 = Vector([0, 0, 1], color=ORANGE, tip_length=0.25)
 
-        self.add(axes, v1, v2, v3)
+        vGroup = VGroup(v1, v2, v3)
+
+        matrix_label = Matrix(matrix)
+        matrix_label_T = MathTex("T=")
+        matrix_label_T.next_to(matrix_label, LEFT)
+
+        label = VGroup(matrix_label, matrix_label_T)
+        label.add_background_rectangle(BLACK, 0.5)
+        label.scale(0.8)
+        self.add_fixed_in_frame_mobjects(label)
+        label.to_corner(UR, buff=1)
+
+        self.add(axes, vGroup)
+        self.wait(2)
         self.move_camera(phi=60 * DEGREES, theta=30 * DEGREES, run_time=2)
         self.wait()
-        self.move_camera(theta=-45 * DEGREES, run_time=2)
-        self.wait()
-        self.begin_ambient_camera_rotation(rate=PI/4)
-        self.wait(5)
+        self.begin_ambient_camera_rotation(rate=PI/8)
+        self.play(vGroup.animate.apply_matrix(matrix), run_time=3)
+        self.play(vGroup.animate.apply_matrix(linalg.inv(matrix)), run_time=3)
+        self.play(vGroup.animate.apply_matrix(matrix), run_time=3)
+        self.play(vGroup.animate.apply_matrix(linalg.inv(matrix)), run_time=3)
+        self.wait(4)
